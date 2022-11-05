694861119af3f27112496c592e4e307393ccfb03
zzzzzzasd123 <112676929+zzzzzzasd123@users.noreply.github.com>
2022-11-05 22:49:28 +0900

    Add files via upload

---
+++ 2200.py
@@ -0,0 +1,36 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = int(input("첫 번째 정수를 입력해주세요 : "))
+... b = int(input("두 번째 정수를 입력해주세요 : "))
+... 
+... print("두 수의 합 : ", a+b)
+... print("두 수의 차 : ", a-b)
+... print("두 수의 곱 : ", a*b)
+... print("두 수의 평균 : ", (a+b)//2)
+... print("두 수 중 큰 수 : ", max(a, b))
+... print("두 수중 작은 수 : ", min(a, b))
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+============== RESTART: C:/Users/Administrator/Desktop/2200264.py ==============
+첫 번째 정수를 입력해주세요 : 10
+두 번째 정수를 입력해주세요 : 20
+두 수의 합 :  30
+두 수의 차 :  -10
+두 수의 곱 :  200
+두 수의 평균 :  15
+두 수 중 큰 수 :  20
+두 수중 작은 수 :  10
+반지름을 입력해주세요 : 10
+높이를 입력해주세요 : 100
+원기둥의 부피 :  31415.92
+정수를 입력해주세요 : 1234
+자리수의 합 :  10
+x1: 0
+y1: 0
+x2: 100
+y2: 100
+두 점 사이의 거리= 141.4213562373095
+현재 시간(영국 그리니치 시각): 13 시 46 분
+물체의 무게를 입력하시오(킬로그램): 10
+물체의 속도를 입력하시오(미터/초): 100
+물체는 50000.0 (줄)의 에너지를 가지고 있다.
